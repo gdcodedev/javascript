8ffd61e341cc301633cf42b2efdad1cd62f85253
Guilherme <guilhermecardoso@uni9.edu.br>
2021-03-03 18:58:04 -0300

    Array

--- js/main.js
+++ js/main.js
@@ -1,10 +1,21 @@
+var lista = ["maça", " pera", "laranja"];
+
+lista.push("Uva"); // Comando push para adicionar um item a lista criada.
+lista.pop(); // Comando utilizado para removar o último item da lista criada.
+
+console.log(lista); 
+
+console.log(lista.length()); // Comando útilizado para verificar quantidade de elementos da lista.
+console.log(list.reverse()); // Comando para reverter.
 
 var nome = " Guilherme Cardoso";
 var idade = 29;
 var  frase = "Japão é o melhor time do mundo";
+
 //alert(nome + " tem " + idade + " anos");
 
 console.log(nome);
 console.log(frase.replace("Japão", "Brasil"));
 
 alert(frase.replace("Japão", "Brasil"));
+
