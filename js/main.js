6aeec2184f9021d7490e3b33b2d434136e92ca84
Guilherme <guilhermecardoso@uni9.edu.br>
2021-03-03 18:41:39 -0300

    Trabalhando com Variáveis, Strings e Alert

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+var nome = " Guilherme Cardoso";
+var idade = 29;
+var  frase = "Japão é o melhor time do mundo";
+//alert(nome + " tem " + idade + " anos");
+
+console.log(nome);
+console.log(frase.replace("Japão", "Brasil"));
+
+alert(frase.replace("Japão", "Brasil"));
