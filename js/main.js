ba8d9324fd6ba75b4aee39db224d4bf89f21d910
Guilherme <guilhermecardoso@uni9.edu.br>
2021-03-03 19:25:17 -0300

    Conhecendo estruturas While, For, Data

--- js/main.js
+++ js/main.js
@@ -1,4 +1,34 @@
-var lista = ["maça", " pera", "laranja"];
+
+
+var d = new Date(); // Conhecendo estrutura Date
+    alert(d);
+
+
+
+
+
+
+
+/*
+var count = 0;   // Estrutura de repetição com o While e For
+while (count < 5){
+    console.log(count);
+    count = count +1;
+}
+
+
+
+var idade = prompt("Qual a sua idade");         //IF E ELSE UTILIZADOS PARA SITUAÇÕES CONDICIONAIS
+if(idade >= 18){                    
+    alert("Maior de idade");
+
+}else{
+    alert("Menor de idade");
+}
+
+
+
+/*var lista = ["maça", " pera", "laranja"];
 
 lista.push("Uva"); // Comando push para adicionar um item a lista criada.
 lista.pop(); // Comando utilizado para removar o último item da lista criada.
@@ -19,3 +49,4 @@ console.log(frase.replace("Japão", "Brasil"));
 
 alert(frase.replace("Japão", "Brasil"));
 
+*/
